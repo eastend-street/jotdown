8eed4b1f9b1eba69c8791e4b96420e5fc59f96ff
eastend-street <ss_1322irf@icloud.com>
2019-03-06 18:58:52 -0800

    make local setting file

--- django/memomemo/memomemo/settings.py
+++ django/memomemo/memomemo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bv1e4i7%8+euflp6n-nm_g*_q8=kpz0x8ksh1i&qm0f##tn)e@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
