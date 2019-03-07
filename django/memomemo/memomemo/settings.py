d5cb42e038787dec026804c67bbd1966c245c97f
eastend-street <ss_1322irf@icloud.com>
2019-03-06 19:04:06 -0800

    remove seceret key in settings.py

--- django/memomemo/memomemo/settings.py
+++ django/memomemo/memomemo/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
