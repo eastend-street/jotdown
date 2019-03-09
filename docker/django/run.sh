5ff6a685a03f9181545c0cab83bf4633101fac34
eastend-street <ss_1322irf@icloud.com>
2019-03-08 18:23:19 -0800

    change directory in run.sh

--- docker/django/run.sh
+++ docker/django/run.sh
@@ -1,4 +1,4 @@
 docker run -it -p 8000:8000 \
-    -v ~/projects/memomemo/django:/memomemo/django \
-    django:latest
+    -v ~/projects/memomemo/django/memomemo:/memomemo/django \
+    django:latest \
     python manage.py runserver 0.0.0.0:8000
