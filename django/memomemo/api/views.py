2a84e06b5d4b865da64e219ddebfb6ac13a8beb5
eastend-street <ss_1322irf@icloud.com>
2019-03-14 23:25:36 -0700

    override list method for test

--- django/memomemo/api/views.py
+++ django/memomemo/api/views.py
@@ -1,6 +1,6 @@
 import django_filters
 from rest_framework import viewsets, filters
-
+from rest_framework.response import Response
 from .models import User, Bookmark
 from .serializers import UserSerializer, BookmarkSerializer
 
@@ -14,3 +14,8 @@ class BookmarkViewSet(viewsets.ModelViewSet):
     queryset = Bookmark.objects.all()
     serializer_class = BookmarkSerializer
     filter_fields = ('user',)
+
+    def list(self, request):
+        print('届いた！')
+        data = BookmarkSerializer(Bookmark.objects.all(), many=True).data
+        return Response(status=200, data=data)
