cc92eb3eef0885f8e963fbb46e1cafa96a33b342
eastend-street <ss_1322irf@icloud.com>
2019-04-25 19:56:33 -0700

    remove bookmarkcard.css, align each bookmarkcard height

--- node/src/components/BookmarkCard/BookmarkCard.js
+++ node/src/components/BookmarkCard/BookmarkCard.js
@@ -8,11 +8,25 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 
-import "./BookmarkCard.css";
+
+const StyledCard = styled(Card)`
+    height: 100%;
+`;
+
+const StyledCardMedia = styled(CardMedia)`
+  && {
+    min-height: 10rem;
+    object-fit: contain;
+  }
+`
 
 const Title = styled(Typography)`
   && {
     font-weight: bold;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+    overflow: hidden;
   }
 `;
 
@@ -20,27 +34,35 @@ const Memo = styled(Typography)`
   && {
     margin-top: 0.5rem;
     word-wrap: break-word;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+    overflow: hidden;
   }
+`;
+
+const StyledHr = styled.hr`
+  border: 0.05rem solid #f5f5f5;
 `;
 
 class BookmarkCard extends Component {
   render() {
     return (
-      <Card>
+      <StyledCard>
         <CardActionArea target="_blank" href={this.props.bookmark.url}>
-          <CardMedia
-            className="media"
+          <StyledCardMedia
             image={this.props.bookmark.img_url}
             title={this.props.bookmark.title}
           />
           <CardContent>
             <Title variant="subheading">{this.props.bookmark.title}</Title>
+            <StyledHr />
             <Memo variant="body1" component="p">
               {this.props.bookmark.memo}
             </Memo>
           </CardContent>
         </CardActionArea>
-      </Card>
+      </StyledCard>
     );
   }
 }
