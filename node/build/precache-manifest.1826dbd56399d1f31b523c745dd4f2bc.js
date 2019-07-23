447fb27f61cae156d1218fad9784ead0da665309
eastend-street <ss_1322irf@icloud.com>
2019-07-22 19:59:03 -0700

    add build directoru

---
+++ node/build/precache-manifest.1826dbd56399d1f31b523c745dd4f2bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d71dbdd46d8a5e92294b9381be57c7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07f8cd2216aff787cad0",
+    "url": "/static/css/main.f65cc47a.chunk.css"
+  },
+  {
+    "revision": "7c288210724dd0c23bd2",
+    "url": "/static/js/2.c14948b9.chunk.js"
+  },
+  {
+    "revision": "07f8cd2216aff787cad0",
+    "url": "/static/js/main.6388e3a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
