c088e12353c2d21f4dba05880662d346d6537f6c
eastend-street <ss_1322irf@icloud.com>
2019-08-20 00:03:47 -0700

    add 404 page

--- node/src/index.js
+++ node/src/index.js
@@ -12,6 +12,8 @@ import BookmarkDetail from "./components/BookmarkDetail/BookmarkDetail";
 import Home from "./components/Home/Home";
 import BookmarkForm from "./components/BookmarkForm/BookmarkForm";
 import Footer from "./components/parts/Footer/Footer";
+import NotFound from "./components/NotFound/NotFound";
+
 import reducer from "./reducers";
 import * as serviceWorker from "./serviceWorker";
 import styled from "styled-components";
@@ -35,6 +37,7 @@ ReactDOM.render(
           <Route path="/" exact component={Home} />
           <Route path="/new" component={BookmarkForm} />
           <Route path="/detail/:id" component={BookmarkDetail} />
+          <Route component={NotFound} />
         </Switch>
       </Content>
       <Footer />
