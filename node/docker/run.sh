ac2b1b26c3a087613b9c07ebb791045310577c4c
eastend-street <suminari.yamada@gmail.com>
2020-05-02 13:24:24 -0700

    changed folder name and refactoring

--- node/docker/run.sh
+++
@@ -1,3 +0,0 @@
-docker run -it -p 3000:3000 \
-    -v ~/projects/jotdown/node:/usr/src/app \
-    node:latest
--- node/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import bookmarks from "./bookmarks";
-import { reducer as form } from "redux-form";
-
-export default combineReducers({ bookmarks, form });
