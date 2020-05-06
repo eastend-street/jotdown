487dbb0914d4df11c0aa07c2be303cd8ffb28e17
eastend-street <suminari.yamada@gmail.com>
2020-05-05 20:52:08 -0700

    renamed django to server folder

--- server/run_local.sh
+++ server/run_local.sh
@@ -1,5 +1,5 @@
 docker run -it -p 8000:8000 \
     --env-file `pwd`/.env_local.sh \
-    -v ~/projects/jotdown/django/jotdown:/jotdown/django \
-    django:latest \
+    -v ~/projects/jotdown/server/jotdown:/jotdown/server \
+    jotdown-server:latest \
     python manage.py runserver 0.0.0.0:8000
--- server/run_prd.sh
+++ server/run_prd.sh
@@ -1,2 +1,2 @@
 docker run -it -p 8000:8000 \
-    django:latest
+    jotdown-server:latest
